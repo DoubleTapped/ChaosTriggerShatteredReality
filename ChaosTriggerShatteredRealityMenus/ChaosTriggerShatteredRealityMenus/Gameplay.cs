998fdd9055c877b75526efc124a0be711ae6a890
Homerun150 <cameronabrams@aol.com>
2017-02-17 09:40:22 -0500

    start of gameplay stuff

--- ChaosTriggerShatteredRealityMenus/ChaosTriggerShatteredRealityMenus/Gameplay.cs
+++ ChaosTriggerShatteredRealityMenus/ChaosTriggerShatteredRealityMenus/Gameplay.cs
@@ -15,6 +15,166 @@ namespace ChaosTriggerShatteredRealityMenus
 {
     class Gameplay : Screen
     {
-        
+        GraphicsDevice graphics;
+        SpriteBatch spriteBatch;
+        private pastPlayerAnimationControll pastPlayerAnimationControll;
+
+        Texture2D atkAnimation1;
+        Texture2D walkRightAnimation;
+        Texture2D walkLeftAnimation;
+        Texture2D atkAnimation2;
+
+        Vector2 startingPoint = new Vector2(200, 200);
+
+        bool isRightKeyDown;
+        bool isLeftKeyDown;
+        bool isAtkKeyDown;
+        bool facingLeft;
+        bool facingRight;
+
+        public Rectangle playerSpriteBounds;
+        public Rectangle enemySpriteBounds;
+
+        public Gameplay()
+        {
+            //graphics = new GraphicsDeviceManager(this);
+            //Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected virtual void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected virtual void LoadContent(ContentManager Content)
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(graphics);
+
+            // TODO: use this.Content to load your game content here
+            atkAnimation1 = Content.Load<Texture2D>("Sword");
+            walkRightAnimation = Content.Load<Texture2D>("Walking");
+            walkLeftAnimation = Content.Load<Texture2D>("Walking2");
+            atkAnimation2 = Content.Load<Texture2D>("sword2");
+
+            pastPlayerAnimationControll = new pastPlayerAnimationControll(atkAnimation1, 1, 2, walkRightAnimation, 1, 3, walkLeftAnimation, 1, 3, atkAnimation2, 1, 2);
+            playerSpriteBounds = new Rectangle((int)startingPoint.X, (int)startingPoint.Y, pastPlayerAnimationControll.TextureCur.Width / pastPlayerAnimationControll.curTextureColumns, pastPlayerAnimationControll.TextureCur.Height / pastPlayerAnimationControll.curTextureRows);
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected void UnloadContent(ContentManager Content)
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected virtual void Update(GameTime gameTime)
+        {
+            Vector2 oldPosition = startingPoint;
+            // Allows the game to exit
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                isRightKeyDown = false;
+                isLeftKeyDown = true;
+                isAtkKeyDown = false;
+                facingRight = false;
+                facingLeft = true;
+                pastPlayerAnimationControll.ChangeCurrentTexture(isRightKeyDown, isLeftKeyDown, isAtkKeyDown, facingRight, facingLeft);
+                startingPoint.X -= 4;
+                pastPlayerAnimationControll.Update();
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                isRightKeyDown = true;
+                isLeftKeyDown = false;
+                isAtkKeyDown = false;
+                facingRight = true;
+                facingLeft = false;
+                pastPlayerAnimationControll.ChangeCurrentTexture(isRightKeyDown, isLeftKeyDown, isAtkKeyDown, facingRight, facingLeft);
+                startingPoint.X += 4;
+                pastPlayerAnimationControll.Update();
+            }
+
+            else if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                startingPoint.Y -= 4;
+            }
+
+            else if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                startingPoint.Y += 4;
+            }
+
+            else if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                isRightKeyDown = false;
+                isLeftKeyDown = false;
+                isAtkKeyDown = true;
+                pastPlayerAnimationControll.ChangeCurrentTexture(isRightKeyDown, isLeftKeyDown, isAtkKeyDown, facingRight, facingLeft);
+                pastPlayerAnimationControll.Update();
+            }
+
+            else
+            {
+                if (isRightKeyDown == true)
+                {
+                    pastPlayerAnimationControll.currentFrame = 1;
+                }
+                else if (isLeftKeyDown == true)
+                {
+                    pastPlayerAnimationControll.currentFrame = 1;
+                }
+                else if (isAtkKeyDown == true)
+                {
+                    pastPlayerAnimationControll.currentFrame = 0;
+                }
+            }
+
+            playerSpriteBounds.X = (int)startingPoint.X;
+            playerSpriteBounds.Y = (int)startingPoint.Y;
+
+            // TODO: Add your update logic here
+            if (!graphics.Viewport.Bounds.Contains(playerSpriteBounds))
+            {
+                startingPoint = oldPosition;
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected virtual void Draw(SpriteBatch spriteBatch)
+        {
+            graphics.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            pastPlayerAnimationControll.Draw(spriteBatch, startingPoint);
+
+            base.Draw(spriteBatch);
+        }
     }
 }
---
+++ ChaosTriggerShatteredRealityMenus/ChaosTriggerShatteredRealityMenus/presentPlayerAnimationControll.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace ChaosTriggerShatteredRealityMenus
+{
+    class presentPlayerAnimationControll
+    {
+       /* public Texture2D TextureAttack1 { get; set; }
+        public Texture2D TextureAttack2 { get; set; }
+        public Texture2D TextureWalkRight { get; set; }
+        public Texture2D TextureWalkLeft { get; set; }
+        public Texture2D TextureCur { get; set; }
+
+        public int AtkRows1 { get; set; }
+        public int AtkColumns1 { get; set; }
+
+        public int AtkRows2 { get; set; }
+        public int AtkColumns2 { get; set; }
+
+        public int WalkRightRows { get; set; }
+        public int WalkRightColumns { get; set; }
+
+        public int WalkLeftRows { get; set; }
+        public int WalkLeftColumns { get; set; }
+
+        public int atkTotalFrames1;
+        public int atkTotalFrames2;
+        public int walkRTotalFrames;
+        public int walkLTotalFrames;
+
+        public int currentFrame;
+        public int curTextureTotalFrames;
+        public int curTextureRows;
+        public int curTextureColumns;
+
+        public int counter = 0;
+
+
+        public animationControll(Texture2D textureAttack, int atkRows, int atkColumns, Texture2D textureWalkRight, int walkRightRows, int walkRightColumns, Texture2D textureWalkLeft, int walkLeftRows, int walkLeftColumns, Texture2D textureAttack2, int atkRows2, int atkColumns2)
+        {
+            TextureAttack1 = textureAttack;
+            AtkRows1 = atkRows;
+            AtkColumns1 = atkColumns;
+            atkTotalFrames1 = AtkRows1 * AtkColumns1;
+
+            TextureAttack2 = textureAttack2;
+            AtkRows2 = atkRows2;
+            AtkColumns2 = atkColumns2;
+            atkTotalFrames2 = AtkRows2 * AtkColumns2;
+
+            TextureWalkRight = textureWalkRight;
+            WalkRightRows = walkRightRows;
+            WalkRightColumns = walkRightColumns;
+            walkRTotalFrames = WalkRightRows * WalkRightColumns;
+
+            TextureWalkLeft = textureWalkLeft;
+            WalkLeftRows = walkLeftRows;
+            WalkLeftColumns = walkLeftColumns;
+            walkLTotalFrames = WalkLeftRows * WalkLeftColumns;
+
+            TextureCur = TextureAttack1;
+            currentFrame = 0;
+
+            curTextureTotalFrames = atkTotalFrames1;
+            curTextureRows = AtkRows1;
+            curTextureColumns = AtkColumns1;
+        }
+
+        public void ChangeCurrentTexture(bool rightKeyDown, bool leftKeyDown, bool atkKeyDown, bool isFacingRight, bool isFacingLeft)
+        {
+            if (rightKeyDown == true && TextureCur != TextureWalkRight)
+            {
+                TextureCur = TextureWalkRight;
+                curTextureRows = WalkRightRows;
+                curTextureColumns = WalkRightColumns;
+                curTextureTotalFrames = walkRTotalFrames;
+            }
+            else if (leftKeyDown == true && TextureCur != TextureWalkLeft)
+            {
+                TextureCur = TextureWalkLeft;
+                curTextureRows = WalkLeftRows;
+                curTextureColumns = WalkLeftColumns;
+                curTextureTotalFrames = walkLTotalFrames;
+            }
+            else if (atkKeyDown == true && (TextureCur != TextureAttack1 || TextureCur != TextureAttack2))
+            {
+                if (isFacingRight == true)
+                {
+                    TextureCur = TextureAttack1;
+                    curTextureRows = AtkRows1;
+                    curTextureColumns = AtkColumns1;
+                    curTextureTotalFrames = atkTotalFrames1;
+                }
+                else if (isFacingLeft == true)
+                {
+                    TextureCur = TextureAttack2;
+                    curTextureRows = AtkRows2;
+                    curTextureColumns = AtkColumns2;
+                    curTextureTotalFrames = atkTotalFrames2;
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        public void Update()
+        {
+            currentFrame++;
+
+            if (currentFrame == curTextureTotalFrames)
+            {
+                currentFrame = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            int width = TextureCur.Width / curTextureColumns;
+            int height = TextureCur.Height / curTextureRows;
+            int row = (int)((float)currentFrame / (float)curTextureColumns);
+            int column = currentFrame % curTextureColumns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(TextureCur, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }*/
+    }
+}
